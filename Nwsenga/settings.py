5fa5610326de27660c116e587b17f63e17ff7854
arkan-leki <arkan.leki@gmail.com>
2024-01-03 16:15:44 +0300

    s

--- Nwsenga/settings.py
+++ Nwsenga/settings.py
@@ -121,7 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATICFILES_DIRS = (
+  os.path.join(SITE_ROOT, 'static/'),
+)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # LOGOUT_REDIRECT_URL = "/"  # new
